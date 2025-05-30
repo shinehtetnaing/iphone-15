038dc89b99e7d8ee0317a18bf13fde57660553df
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-05-30 09:39:02 +0700

    feat: highlights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import Highlights from "@/components/Highlights";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="bg-black">
       <Navbar />
       <Hero />
+      <Highlights />
     </main>
   );
 }
